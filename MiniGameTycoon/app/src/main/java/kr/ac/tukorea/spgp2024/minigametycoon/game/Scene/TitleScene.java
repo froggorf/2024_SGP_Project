be60bbf5339d05e72c2d9997ebbe8e691cd9f24c
Lee Yoon Seok <kjggr123@gmail.com>
2024-04-10 01:52:23 +0900

    타이틀 씬으로 이동하는 로직 구현
    현재는 타이틀씬에서 메인 음악만 재생하는 상태

--- MiniGameTycoon/app/src/main/java/kr/ac/tukorea/spgp2024/minigametycoon/game/Scene/TitleScene.java
+++ MiniGameTycoon/app/src/main/java/kr/ac/tukorea/spgp2024/minigametycoon/game/Scene/TitleScene.java
@@ -5,32 +5,24 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
-import java.security.acl.Owner;
-
 import kr.ac.tukorea.spgp2024.R;
 import kr.ac.tukorea.spgp2024.minigametycoon.framework.objects.Sprite;
 import kr.ac.tukorea.spgp2024.minigametycoon.framework.res.Sound;
 import kr.ac.tukorea.spgp2024.minigametycoon.framework.scene.BaseScene;
 import kr.ac.tukorea.spgp2024.minigametycoon.framework.view.Metrics;
 
-public class LogoScene extends BaseScene {
+public class TitleScene extends BaseScene {
     private final String TAG = LogoScene.class.getSimpleName();
-    Context OwnerContext;
     Sprite LogoSprite;
     public enum Layer{
-        LOGO, COUNT
+        TITLE,  COUNT
     }
-    public LogoScene(Context context, Bundle extras) {
-        // 게임 비율 설정
-        Metrics.setGameSize(9.0f, 16.0f);
-
-        OwnerContext=context;
+    public TitleScene() {
         // 레이어 초기화
         initLayers(Layer.COUNT);
 
-        LogoSprite = new Sprite(R.mipmap.tukorea_logo,4.5f,8.0f,8.0f,2.0f);
-        Log.d(TAG,"111");
-        add(Layer.LOGO,LogoSprite);
+        LogoSprite = new Sprite(R.mipmap.title,4.5f,8.0f,9.0f,16.0f);
+        add(Layer.TITLE,LogoSprite);
 
     }
 
@@ -44,21 +36,7 @@ public class LogoScene extends BaseScene {
         // 500ms 뒤 노래 틀기
         new Handler().postDelayed(new Runnable(){
             public void run(){
-                Sound.playMusic(R.raw.logo_title);
-                // 1s 뒤 노래 끄기
-                new Handler().postDelayed(new Runnable(){
-                    public void run(){
-                        Sound.stopMusic();
-                        // 타이틀 씬으로 넘어가기
-                        new Handler().postDelayed(new Runnable(){
-                            public void run(){
-                                BaseScene titleScene = new TitleScene();
-                                titleScene.changeScene();
-                            }
-                        }, 500);
-
-                    }
-                }, 1000);
+                Sound.playMusic(R.raw.title_sound);
 
             }
         }, 500);
