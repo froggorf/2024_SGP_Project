dc8408a2649e0ddb4dfa191912de375906a1c30d
Lee Yoon Seok <kjggr123@gmail.com>
2024-04-14 01:58:46 +0900

    재료준비소 게임에서 오브젝트를 자르기 위한 CutLineObject 생성 (IGameObject 인터페이스 상속받아 draw / update 함수오버라이드)

---
+++ MiniGameTycoon/app/src/main/java/kr/ac/tukorea/spgp2024/minigametycoon/game/Object/FoodPrepHouseGame/CutLineObject.java
@@ -0,0 +1,57 @@
+package kr.ac.tukorea.spgp2024.minigametycoon.game.Object.FoodPrepHouseGame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.graphics.Typeface;
+import android.graphics.drawable.shapes.Shape;
+import android.util.AttributeSet;
+
+import kr.ac.tukorea.spgp2024.minigametycoon.framework.interfaces.IGameObject;
+import kr.ac.tukorea.spgp2024.minigametycoon.framework.view.GameView;
+
+// 재료 준비소에서 1차 재료를 자를 때 사용할 선 오브젝트
+public class CutLineObject implements IGameObject {
+    private float startX;
+    private float startY;
+
+    private float endX;
+    private float endY;
+    private Paint paint;
+    static DashPathEffect dashPath = new DashPathEffect(new float[]{10,10}, 5);
+    public CutLineObject(){
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(5.0f);
+        paint.setPathEffect(dashPath);
+
+        startX = startY = endX = endY = -1;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawLine(startX,startY,endX,endY, paint);
+    }
+
+    public void setStartPos(float x, float y)
+    {
+        startX = x;
+        startY = y;
+    }
+
+    public void setEndPos(float x, float y)
+    {
+        endX = x;
+        endY = y;
+    }
+
+}
